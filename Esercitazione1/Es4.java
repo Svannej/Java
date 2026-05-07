0da59c8549f980d1396de880a43d38e07e0314d8
Vanni <gio.alioto84@gmail.com>
2026-05-07 12:20:14 +0200

    esercitazione 1 fino a 4

---
+++ Esercitazione1/Es4.java
@@ -0,0 +1,14 @@
+package Esercitazione1;
+
+public class Es4 {
+
+    public static void main(String args[]){    
+        int gemini = 0;
+
+        for(int i = 50; i <= 100; i++){
+            gemini = gemini + i;
+        }
+
+        System.out.println("la somma è: " + gemini);
+    }
+}
